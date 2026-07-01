fd5ae6cfab201e99a2c0b3de0c7abe4dd50c20b8
zeroNhatty <ntnesi01@gmail.com>
2026-07-01 20:57:06 +0300

    feat(app): Hero section added

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,15 @@
 <template>
   <Header/>
+  <!--This is the Hero Section-->
+  <div class="hero bg-base-200 min-h-screen">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">Welcome to PGM</h1>
+        <p class="py-6">
+          This is a theoretical simulator for a power grid system power status and fault pinpointing.
+        </p>
+        <button class="btn btn-primary">Check Status</button>
+      </div>
+    </div>
+  </div>
 </template>
--- app/components/Header.vue
+++ app/components/Header.vue
@@ -5,7 +5,7 @@
 <template>
   <div class="navbar bg-base-100 shadow-sm">
     <div class="navbar-start">
-      <a class="btn btn-ghost text-xl">daisyUI</a>
+      <a class="btn btn-ghost text-xl">PGM</a>
     </div>
 
     <div class="navbar-end">
