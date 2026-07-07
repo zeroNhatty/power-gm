e0096450188a412937e26da78d00190103a65ae5
zeroNhatty <ntnesi01@gmail.com>
2026-07-07 20:56:01 +0300

    feat: retrive power node data

--- app/pages/operational/dashboard.vue
+++ app/pages/operational/dashboard.vue
@@ -3,11 +3,22 @@ definePageMeta({
   layout: 'operational-logged-in',
   middleware: ['sanctum:auth'],
 });
+
+const client = useSanctumClient()
+const { data, error, pending } = await useAsyncData('nodes', () =>
+    client('/api/nodes'));
 </script>
 
 <template>
-  works ig
 <NuxtLink href="profile">Profile</NuxtLink>
+
+  <div>
+    <div v-if="pending">Loading nodes...</div>
+    <div v-else-if="error">Error loading data: {{ error.message }}</div>
+    <ul v-else>
+      <li v-for="node in data" >{{ node }}</li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
