4040bd95363d7717bd2cc3d828e2bc73928377d4
zeroNhatty <ntnesi01@gmail.com>
2026-07-28 14:38:57 +0300

    feat(ticket): ticket resolvable

--- app/components/TicketModal.vue
+++ app/components/TicketModal.vue
@@ -3,7 +3,7 @@ import type { Ticket } from '~/pages/operational/tickets.vue';
 
 const user = useUserSession()['user']
 const client = useSanctumClient(); 
-const emit = defineEmits(['claimed']);
+const emit = defineEmits(['claimed', 'solved']);
 
 const props = defineProps<{
   ticket: Ticket | null;
@@ -40,6 +40,30 @@ defineExpose({
     console.error('Failed to claim ticket:', err)
   }
 }
+
+async function ticketResolve() {
+  if (!props.ticket || !user.value?.id) return
+
+  try {
+    const updatedTicket = await client(`/api/tickets/${props.ticket.id}`, {
+      method: 'PUT',
+      body: {
+        assignee_id: props.ticket.assignee_id ?? user.value.id,
+        status: 'solved',
+      },
+    })
+
+    console.log('Ticket marked solved successfully:', updatedTicket)
+
+    const modalEl = document.getElementById('someMo') as HTMLDialogElement | null
+    modalEl?.close()
+
+    emit('solved')
+
+  } catch (err) {
+    console.error('Failed to solve ticket:', err)
+  }
+}
 </script>
 
 <template>
@@ -63,6 +87,9 @@ defineExpose({
         <form method="dialog">
           <button v-if="props.ticket.status === 'pending'" class="btn btn-primary mr-2" @click="technicianClaim">
             Handle Issue
+          </button>
+          <button v-if="props.ticket.status === 'assigned' && user?.id === props.ticket.assignee_id" class="btn btn-primary mr-2" @click="ticketResolve">
+            Mark as Solved
           </button>
           <button class="btn">Close</button>
         </form>
