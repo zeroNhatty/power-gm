ff4fddb31b385465cff267c49a2b5e20856a5f04
zeroNhatty <ntnesi01@gmail.com>
2026-07-28 14:27:40 +0300

    feat(ticket): technican can claim a ticket

--- app/components/TicketModal.vue
+++ app/components/TicketModal.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import type { Ticket } from '~/pages/operational/tickets.vue';
 
+const user = useUserSession()['user']
+const client = useSanctumClient(); 
+const emit = defineEmits(['claimed']);
+
 const props = defineProps<{
   ticket: Ticket | null;
 }>();
@@ -11,7 +15,31 @@ const dialogRef = ref<HTMLDialogElement | null>(null);
 defineExpose({
   showModal: () => dialogRef.value?.showModal(),
   close: () => dialogRef.value?.close(),
-});
+})
+
+ async function technicianClaim() {
+  if (!props.ticket || !user.value?.id) return
+
+  try {
+    const updatedTicket = await client(`/api/tickets/${props.ticket.id}`, {
+      method: 'PUT',
+      body: {
+        assignee_id: user.value.id,
+        status: 'assigned',
+      },
+    })
+
+    console.log('Ticket claimed successfully:', updatedTicket)
+
+    const modalEl = document.getElementById('someMo') as HTMLDialogElement | null
+    modalEl?.close()
+
+    emit('claimed')
+
+  } catch (err) {
+    console.error('Failed to claim ticket:', err)
+  }
+}
 </script>
 
 <template>
@@ -33,7 +61,7 @@ defineExpose({
 
       <div class="modal-action">
         <form method="dialog">
-          <button v-if="props.ticket.status === 'pending'" class="btn btn-primary mr-2">
+          <button v-if="props.ticket.status === 'pending'" class="btn btn-primary mr-2" @click="technicianClaim">
             Handle Issue
           </button>
           <button class="btn">Close</button>
